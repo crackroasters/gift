23754c7eea3808a1a65b03468718214368edb65b
crackroasters <crackroasters@naver.com>
2026-02-13 14:32:40 +0000

    tet

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function launchCelebration(type = "heart") {
 		const x = Math.random() * 100
 		const delay = Math.random() * 0.3
 		const size = 12 + Math.random() * 12
-		const duration = 2 + Math.random()
+		const duration = 100 + Math.random()
 
 		particle.style.left = `${x}vw`
 		particle.style.animationDelay = `${delay}s`
